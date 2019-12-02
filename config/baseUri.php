6d2cda3d8612be1f5d73f4c847d6475f12f75348
Mehran Hassanzadeh <hassanzadeh.me@gmail.com>
2019-12-02 10:48:05 +0330

    v3.0.0

--- config/baseUri.php
+++ config/baseUri.php
@@ -1,15 +1,15 @@
 <?php
 return [
     'Sandbox' => [
-        'SSO-ADDRESS'                   =>  'https://accounts.pod.land/',
-        'PLATFORM-ADDRESS'              =>  'http://sandbox.pod.land/srv/basic-platform/',
-        'PRIVATE-CALL-ADDRESS'          =>  'https://sandbox.pod.land:1033/',
-        'FILE-SERVER-ADDRESS'           =>  'http://sandbox.pod.land:8080/',
+        'SSO-ADDRESS'                   =>  'https://accounts.pod.ir/',
+        'PLATFORM-ADDRESS'              =>  'http://sandbox.pod.ir/srv/basic-platform/',
+        'PRIVATE-CALL-ADDRESS'          =>  'https://sandbox.pod.ir:1033/',
+        'FILE-SERVER-ADDRESS'           =>  'http://sandbox.pod.ir:8080/',
     ],
     'Production' => [
-        'SSO-ADDRESS'                   =>  'https://accounts.pod.land/',
-        'PLATFORM-ADDRESS'              =>  'https://api.pod.land/srv/core/',
-        'PRIVATE-CALL-ADDRESS'          =>  'https://pay.pod.land/',
-        'FILE-SERVER-ADDRESS'           =>  'https://core.pod.land/',
+        'SSO-ADDRESS'                   =>  'https://accounts.pod.ir/',
+        'PLATFORM-ADDRESS'              =>  'https://api.pod.ir/srv/core/',
+        'PRIVATE-CALL-ADDRESS'          =>  'https://pay.pod.ir/',
+        'FILE-SERVER-ADDRESS'           =>  'https://core.pod.ir/',
     ]
 ];
