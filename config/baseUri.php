6e993c7c715dc42f80406468943baa628b291b4a
Elham Keshtgar <el.keshtgar@gmail.com>
2020-08-27 13:19:27 +0430

    change platform-address

--- config/baseUri.php
+++ config/baseUri.php
@@ -2,13 +2,13 @@
 return [
     'Sandbox' => [
         'SSO-ADDRESS'                   =>  'https://accounts.pod.ir/',
-        'PLATFORM-ADDRESS'              =>  'http://sandbox.pod.ir/srv/basic-platform/',
+        'PLATFORM-ADDRESS'              =>  'http://sandbox.pod.ir/srv/sc/',
         'PRIVATE-CALL-ADDRESS'          =>  'https://sandbox.pod.ir:1033/',
         'FILE-SERVER-ADDRESS'           =>  'http://sandbox.pod.ir:8080/',
     ],
     'Production' => [
         'SSO-ADDRESS'                   =>  'https://accounts.pod.ir/',
-        'PLATFORM-ADDRESS'              =>  'https://api.pod.ir/srv/core/',
+        'PLATFORM-ADDRESS'              =>  'https://api.pod.ir/srv/sc/',
         'PRIVATE-CALL-ADDRESS'          =>  'https://pay.pod.ir/',
         'FILE-SERVER-ADDRESS'           =>  'https://core.pod.ir/',
         'SAKKU-API-ADDRESS'             =>  'https://api.sakku.cloud/',
